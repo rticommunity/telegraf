51058bb176af7d9ffa57b083af2fb964dc978b4b
Kyoungho An <kyoungho.an@gmail.com>
2018-11-16 09:51:51 -0800

    added timestamp in metrics

--- plugins/outputs/dds_producer/dds_producer.go
+++ plugins/outputs/dds_producer/dds_producer.go
@@ -11,23 +11,24 @@
 package dds_producer
 
 import (
-  "github.com/influxdata/telegraf"
-  "github.com/influxdata/telegraf/plugins/outputs"
-  "github.com/influxdata/telegraf/plugins/serializers"
-  "github.com/rticommunity/rticonnextdds-connector-go"
-  "fmt"
+	"fmt"
+	"time"
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/outputs"
+	"github.com/influxdata/telegraf/plugins/serializers"
+	"github.com/rticommunity/rticonnextdds-connector-go"
 )
 
 type DDSProducer struct {
-  // XML configuration file path
-  ConfigFilePath    string `toml:"config_path"`
-  ParticipantConfig string `toml:"participant_config"`
-  WriterConfig      string `toml:"writer_config"`
+	// XML configuration file path
+	ConfigFilePath    string `toml:"config_path"`
+	ParticipantConfig string `toml:"participant_config"`
+	WriterConfig      string `toml:"writer_config"`
 
-  connector *rti.Connector
-  writer      *rti.Output
+	connector *rti.Connector
+	writer    *rti.Output
 
-  serializer serializers.Serializer
+	serializer serializers.Serializer
 }
 
 var sampleConfig = `
@@ -48,133 +49,137 @@ data_format = "json"
 `
 
 func (d *DDSProducer) SetSerializer(serializer serializers.Serializer) {
-  d.serializer = serializer
+	d.serializer = serializer
 }
 
 func (d *DDSProducer) Connect() error {
-  var err error
+	var err error
 
-  // Create DDS entities
-  d.connector, err = rti.NewConnector(d.ParticipantConfig, d.ConfigFilePath)
-  if err != nil {
-    return err
-  }
+	// Create DDS entities
+	d.connector, err = rti.NewConnector(d.ParticipantConfig, d.ConfigFilePath)
+	if err != nil {
+		return err
+	}
 
-  d.writer, err = d.connector.GetOutput(d.WriterConfig)
-  if err != nil {
-    return err
-  }
+	d.writer, err = d.connector.GetOutput(d.WriterConfig)
+	if err != nil {
+		return err
+	}
 
-  return nil
+	return nil
 }
 
 func (d *DDSProducer) Close() error {
-  d.connector.Delete()
-  return nil
+	d.connector.Delete()
+	return nil
 }
 
 func (d *DDSProducer) SampleConfig() string {
-  return sampleConfig
+	return sampleConfig
 }
 
 func (d *DDSProducer) Description() string {
-  return "Send metrics to DDS"
+	return "Send metrics to DDS"
 }
 
 func (d *DDSProducer) Write(metrics []telegraf.Metric) error {
-  if len(metrics) == 0 {
-    return nil
-  }
+	if len(metrics) == 0 {
+		return nil
+	}
 
-  for _, metric := range metrics {
-    err := d.writer.Instance.SetString("name", metric.Name())
-    if err != nil {
-      return err
-    }
+	for _, metric := range metrics {
+		err := d.writer.Instance.SetString("name", metric.Name())
+		if err != nil {
+			return err
+		}
 
-    for i, tag := range metric.TagList() {
-      key := fmt.Sprintf("tags[%d].key", i+1)
-      value := fmt.Sprintf("tags[%d].value", i+1)
-      err = d.writer.Instance.SetString(key, tag.Key)
-      if err != nil {
-        return err
-      }
-      err = d.writer.Instance.SetString(value, tag.Value)
-      if err != nil {
-        return err
-      }
-    }
-    for i, field := range metric.FieldList() {
-      key := fmt.Sprintf("fields[%d].key", i+1)
-      value := fmt.Sprintf("fields[%d].value", i+1)
-      err = d.writer.Instance.SetString(key, field.Key)
-      if err != nil {
-        return err
-      }
+		for i, tag := range metric.TagList() {
+			key := fmt.Sprintf("tags[%d].key", i+1)
+			value := fmt.Sprintf("tags[%d].value", i+1)
+			err = d.writer.Instance.SetString(key, tag.Key)
+			if err != nil {
+				return err
+			}
+			err = d.writer.Instance.SetString(value, tag.Value)
+			if err != nil {
+				return err
+			}
+		}
+		for i, field := range metric.FieldList() {
+			key := fmt.Sprintf("fields[%d].key", i+1)
+			value := fmt.Sprintf("fields[%d].value", i+1)
+			err = d.writer.Instance.SetString(key, field.Key)
+			if err != nil {
+				return err
+			}
 
-      v := convertField(field.Value)
+			v := convertField(field.Value)
 
-      err = d.writer.Instance.SetFloat64(value, v)
-      if err != nil {
-        return err
-      }
+			err = d.writer.Instance.SetFloat64(value, v)
+			if err != nil {
+				return err
+			}
 
-    }
+		}
 
-    err = d.writer.Write()
-    if err != nil {
-      return err
-    }
+		d.writer.Instance.SetInt64("timestamp", time.Now().UTC().UnixNano())
+		if err != nil {
+			return err
+		}
 
-    err = d.writer.ClearMembers()
-    if err != nil {
-      return err
-    }
-  }
-  return nil
+		err = d.writer.Write()
+		if err != nil {
+			return err
+		}
+
+		err = d.writer.ClearMembers()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // Convert field to a supported type or nil if unconvertible
 func convertField(v interface{}) float64 {
-  switch v := v.(type) {
-  case float64:
-    return v
-  case int64:
-    return float64(v)
-    //case string:
-    //  return v
-    //case bool:
-    //  return float64(v)
-  case int:
-    return float64(v)
-  case uint:
-    return float64(v)
-  case uint64:
-    return float64(v)
-    //case []byte:
-    //  return float64(v)
-  case int32:
-    return float64(v)
-  case int16:
-    return float64(v)
-  case int8:
-    return float64(v)
-  case uint32:
-    return float64(v)
-  case uint16:
-    return float64(v)
-  case uint8:
-    return float64(v)
-  case float32:
-    return float64(v)
-  default:
-    return 0
-  }
+	switch v := v.(type) {
+	case float64:
+		return v
+	case int64:
+		return float64(v)
+		//case string:
+		//  return v
+		//case bool:
+		//  return float64(v)
+	case int:
+		return float64(v)
+	case uint:
+		return float64(v)
+	case uint64:
+		return float64(v)
+		//case []byte:
+		//  return float64(v)
+	case int32:
+		return float64(v)
+	case int16:
+		return float64(v)
+	case int8:
+		return float64(v)
+	case uint32:
+		return float64(v)
+	case uint16:
+		return float64(v)
+	case uint8:
+		return float64(v)
+	case float32:
+		return float64(v)
+	default:
+		return 0
+	}
 }
 
-
 func init() {
-  outputs.Add("dds_producer", func() telegraf.Output {
-    return &DDSProducer{}
-  })
+	outputs.Add("dds_producer", func() telegraf.Output {
+		return &DDSProducer{}
+	})
 }
